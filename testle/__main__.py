ae7f84c9591545dd7cf0427b8d147e95550ddea3
Your Name <mahmoud.m.y234@gmail.com>
2023-01-05 15:27:55 +0200

    Fixing .gitignore problem about testle ‘__pycache__’

--- testle/__main__.py
+++ testle/__main__.py
@@ -8,6 +8,13 @@ def __main__(cmd="help", *args):
 			print("create: creates a new testlang project.")
 		case "create":
 			print(f"Directory ‘{args[0]}’ will be created/wiped to create project, continue? [Y/n]")
+			while (choice := input().lower()) not in "yn":
+				choice = input("Continue? [Y/n]")
+				if choice == "n":
+					print("Terminating...")
+					return # Go outside the function
+				else:
+					break # Go outside the loop
 			print("Creating project...")
 
 if __name__ == "__main__":
